3de85077caaea892775b03998c57516c7178b21b
Chuthinthon <chuthinthon.nkz@gmail.com>
2019-07-07 00:12:23 +0700

    initial project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component'
+import { DanceComponent } from './dance/dance.component'
+import { ClassComponent } from './class/class.component'
+import { VideoComponent } from './video/video.component'
+import { ContactComponent } from './contact/contact.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'dance', component: DanceComponent},
+  {path: 'class', component: ClassComponent},
+  {path: 'video', component: VideoComponent},
+  {path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DanceComponent } from './dance/dance.component';
+import { VideoComponent } from './video/video.component';
+import { ClassComponent } from './class/class.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    DanceComponent,
+    VideoComponent,
+    ClassComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
